03c0d72b3bc6ddfe5af148ce9084afae2a25a677
sanjay <sanjaymithra319@gmail.com>
2026-08-19 11:30:00 +0530

    Implement global master kill-switch toggle with pulsating glow animation

--- popup.js
+++ popup.js
@@ -1,3 +1,14 @@
 (function () {
-  console.log("Repaint popup initialized");
+  let masterEnabled = true;
+  const masterKillSwitch = document.getElementById("masterKillSwitch");
+
+  masterKillSwitch.addEventListener("change", () => {
+    masterEnabled = masterKillSwitch.checked;
+    chrome.storage.local.set({ masterEnabled });
+  });
+
+  chrome.storage.local.get(["masterEnabled"], (store) => {
+    masterEnabled = store.masterEnabled !== undefined ? store.masterEnabled : true;
+    masterKillSwitch.checked = masterEnabled;
+  });
 })();
